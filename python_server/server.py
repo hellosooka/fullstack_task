6633e8c7ee9849b319a9c21ae28099b38e6da1c0
Hellosooka <leo.zemtsoff@icloud.com>
2022-09-24 23:21:58 +0300

    add python

--- python_server/server.py
+++ python_server/server.py
@@ -1,9 +1,33 @@
-import socket
+from aiohttp import web
+import socketio
 
 
-new_socket = socket.socket()
-new_socket.bind(("127.0.0.1", 2000))
-new_socket.listen()
+sio = socketio.AsyncServer(cors_allowed_origins=['http://localhost:3000'])
+
+app = web.Application()
+
+sio.attach(app)
+
+async def index(request):
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
 
 
-print('Server started')
+@sio.on('*')
+def catch_all(event, sid, data):
+    sio.emit('send_message', { 'data' : 'data' })
+
+@sio.on('message')
+async def print_message(sid, message):
+    print("Socket ID: " , sid)
+    print(message)
+    
+
+
+
+
+app.router.add_get('/', index)
+
+
+if __name__ == '__main__':
+    web.run_app(app)
